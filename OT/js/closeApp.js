cff154c2622ba823d906f03504eedf811b6894f6
root <root@localhost.localdomain>
2016-04-28 17:06:58 -0500

    ajustes ok

--- OT/js/closeApp.js
+++
@@ -1,5 +0,0 @@
-  timeNow = new Date();
-    closeApp = new Date(timeNow.getFullYear(),timeNow.getMonth(), timeNow.getDate(), 0, 0).getTime() - Date.now();
-    setTimeout(function(){
-        logout();
-    }, closeApp);
